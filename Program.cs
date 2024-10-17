f9699e8d8a1ae71c58da9ec7b346166533b7f820
Kaushik <Kaushik@LAPTOP-I3IMPBJS>
2024-10-17 21:47:52 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿//Create a project to demonstrate the working of command line arguments for constant and read only using command prompt.
+using System;
+namespace LAB
+{
+    class Program1
+    {
+        public static void Main(string [] args)
+        {
+            for(int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine(args[i]);
+            }
+            Console.ReadKey();
+        }
+
+        
+    }
+}
